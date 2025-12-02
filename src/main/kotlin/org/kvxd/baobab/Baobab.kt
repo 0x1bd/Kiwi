32cdf2ebe95cd3214448ee6a049b6dbd85e8fe28
kvxd <redslyice@outlook.com>
2025-12-02 20:21:14 +0100

    chore: initial commit

---
+++ src/main/kotlin/org/kvxd/baobab/Baobab.kt
@@ -0,0 +1,9 @@
+package org.kvxd.baobab
+
+import net.fabricmc.api.ModInitializer
+
+class Baobab : ModInitializer {
+
+    override fun onInitialize() {
+    }
+}
---
+++ src/main/kotlin/org/kvxd/baobab/client/BaobabClient.kt
@@ -0,0 +1,9 @@
+package org.kvxd.baobab.client
+
+import net.fabricmc.api.ClientModInitializer
+
+class BaobabClient : ClientModInitializer {
+
+    override fun onInitializeClient() {
+    }
+}
